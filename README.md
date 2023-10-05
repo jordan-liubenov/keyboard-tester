2ab0731c6289ee5c3a851c880e050dc24e145d44
jordan-liubenov <jordan.liubenov62@gmail.com>
2023-10-05 17:09:54 +0300

    update readme

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Application used for testing inputs on your keyboard.
 Made using React/Typescript and Electron to package into a desktop application.
 
 ![alt text](https://i.ibb.co/nL9V2TQ/image.png)
+
+# Features so far
+
+-Visualization of all Alpha Keys + Spacebar
+-Input history
+-Input history clearing
+
+# Upcoming features
+
+-Visualization of all Mod keys + Num Keys
--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -52,8 +52,8 @@ const Keyboard = (props: KeyboardProps) => {
         })}
       </div>
       <div className="bottomRow">
-        {/* <div className="LCtrl">Ctr</div>
-        <div className="Win">Win</div> */}
+        <div className="LCtrl">Ctr</div>
+        <div className="Win">Win</div>
         <div className="LAlt">Alt</div>
         <div className="RAlt">Alt</div>
         <div className="spacebar">{"\0"}</div>
